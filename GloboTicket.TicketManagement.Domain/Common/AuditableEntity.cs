0d846b37cd6c9d5fb1e92afceb8518905a994077
Bruno Dos Santos <brunosantos.dev@outlook.com>
2021-06-28 20:00:20 +0200

    Add project files.

---
+++ GloboTicket.TicketManagement.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GloboTicket.TicketManagement.Domain.Common
+{
+    // Class to track data changes
+    public class AuditableEntity
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+The goal of the Core part is to achieve less coupling as possible by using
+- Contracts: Interfaces, repositories (which will handle data access operations)
+- Messaging
