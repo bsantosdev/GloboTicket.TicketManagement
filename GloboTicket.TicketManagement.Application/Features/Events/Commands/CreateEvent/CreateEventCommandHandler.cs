78aec70baff8caf6e4d4e61fa5f6dc985135f136
Bruno Dos Santos <brunosantos.dev@outlook.com>
2021-07-12 20:28:12 +0200

    Introduced email service to send email whenever an event is created

--- GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using FluentValidation.Results;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
 using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Application.Models.Mail;
 using GloboTicket.TicketManagement.Domain.Entities;
 using MediatR;
 using System;
@@ -13,13 +15,16 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
     {
         private readonly IEventRepository _eventRepository;
         private readonly IMapper _mapper;
+        private readonly IEmailService _emailService;
 
         public CreateEventCommandHandler(
             IMapper mapper, 
-            IEventRepository eventRepository)
+            IEventRepository eventRepository,
+            IEmailService emailService)
         {
             _mapper = mapper;
             _eventRepository = eventRepository;
+            _emailService = emailService;
         }
 
         public async Task<Guid> Handle(
@@ -37,6 +42,23 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
             Event itemToAdd = _mapper.Map<Event>(request);
 
             itemToAdd = await _eventRepository.AddAsync(itemToAdd);
+
+            Email email = new Email() 
+            { 
+                To = "brunosantos.dev@outlook.com", 
+                Body = $"A new event was created: {request}", 
+                Subject = "A new event was created" 
+            };
+
+            try
+            {
+                await _emailService.SendEmail(email);
+            }
+            catch (Exception ex)
+            {
+                // ToDo: Add logging here
+                // this shoudn't stop the API from doing else so this can be logged
+            }
 
             return itemToAdd.EventId;
         }
---
+++ GloboTicket.TicketManagement.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,48 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
+using GloboTicket.TicketManagement.Application.Models.Mail;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Infrastructure.Mail
+{
+    public class EmailService : IEmailService
+    {
+        public EmailSettings _emailSettings { get; set; }
+
+        public EmailService(IOptions<EmailSettings> mailSettings)
+        {
+            _emailSettings = mailSettings.Value;
+        }
+
+        public async Task<bool> SendEmail(Email email)
+        {
+            SendGridClient client = new SendGridClient(_emailSettings.ApiKey);
+
+            string subject = email.Subject;
+            EmailAddress to = new EmailAddress(email.To);
+            string emailBody = email.Body;
+
+            EmailAddress from = new EmailAddress
+            {
+                Email = _emailSettings.FromAddress,
+                Name = _emailSettings.FromName
+            };
+
+            SendGridMessage sendGridMessage = MailHelper
+                .CreateSingleEmail(from, to, subject, emailBody, emailBody);
+
+            Response response = await client.SendEmailAsync(sendGridMessage);
+
+            if (response.StatusCode == HttpStatusCode.Accepted 
+                || response.StatusCode == HttpStatusCode.OK)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
